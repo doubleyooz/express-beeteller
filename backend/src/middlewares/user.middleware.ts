3ccfe533fe5a2083c4f5b3434a3fa7e912c8999e
Jojo <sirjoandersonwf@gmail.com>
2022-03-16 18:17:53 -0300

    add: user.middleware store()

---
+++ backend/src/middlewares/user.middleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import * as yup from 'yup';
+import { getMessage } from '../utils/message.util';
+
+const rules = {
+    email: yup.string().email(),
+    password: yup
+    .string()
+    .min(8, getMessage('user.invalid.password.short'))
+    .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        getMessage('user.invalid.password.weak'))
+};
+
+async function store(req: Request, res: Response, next: NextFunction) {
+    const yupObject = yup.object().shape({
+        email: rules.email.required(),       
+        password: rules.password.required(),
+    });
+
+
+    yupObject
+        .validate(req.body, { stripUnknown: true })
+        .then(() => next())
+        .catch((err: any) => {
+            return res.status(400).json({
+                message: getMessage('default.badRequest'),
+                data: err.errors,
+            });           
+        });
+
+}
+
+export default { store };
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response } from 'express';
 import UserController from '../controllers/user.controller';
+import UserMiddleware from '../middlewares/user.middleware';
 
 const routes = Router();
 
-routes.post('/users', UserController.store);
+routes.post('/users', UserMiddleware.store, UserController.store);
 
 export default routes;
