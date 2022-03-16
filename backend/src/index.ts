2e2ba3fcf1981ec5e2db6367386c59e31c29fc10
Jojo <sirjoandersonwf@gmail.com>
2022-03-16 16:11:29 -0300

    add: mongoose connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config'
+import mongoose from 'mongoose';
 import { app } from './config/express.config';
 
+mongoose.connect(`${process.env.DB_CONNECTION}`);
 const server = require('http').Server(app);
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
