b30db0192d049328fac3ce112ee073f559a1cb0f
Jojo <sirjoandersonwf@gmail.com>
2022-03-18 23:56:43 -0300

    add: login card and basic stylings

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React from 'react';
+import './styles.scss';
 
 const LoginPage = () => {
     return (
-        <div>
-            Login
+        <div className="login-container">
+            <div className="image"></div>
+            <div className="card">
+                <div className="header">
+                    <h3>Olá! Bem vindo de volta.</h3>
+                    <h5>Faça Login com seus dados inseridos durante o registro</h5>
+                </div>
+                <form action="">
+                    <label htmlFor="email">email</label>
+                    <input type="email" name="email" placeholder="email" />
+                    <label htmlFor="password">password</label>
+                    <input type="password" name="password" placeholder="password" />
+                    <input type="submit" value="Login" />
+                </form>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default LoginPage;
