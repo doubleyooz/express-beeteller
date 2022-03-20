a81a5a0b479433bfa1dbabbcb3cbd812dfc98603
Jojo <sirjoandersonwf@gmail.com>
2022-03-20 11:40:41 -0300

    add: horizontal bar

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import HorizontalBar from "./components/HorizontalBar";
 
 const Paths = () => {
   return (
     <div className="routes-container">
       <BrowserRouter>
+      <HorizontalBar/>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
