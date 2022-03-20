bc41b4e2a7c2557d1f8d4a6d0b1639f60fb31c90
Jojo <sirjoandersonwf@gmail.com>
2022-03-20 16:48:18 -0300

    update: dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import Box from '../../components/Box';
+import './styles.scss';
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    return (
+        <div className="dashboard-container">
+            <div className="card-container">
+                <div className="header">
+                    <span className="title">MOEDAS</span>
+                </div>
+                
+                <div className="cards">
+                    <Box
+                        name="BRL/USD"
+                        price="5,30"
+                        description="Dolar Turismo"
+                    />
+                    <Box name="BTC/EUR" price="3732,09" description="Euro" />
+                    <Box
+                        name="BTC/USD"
+                        price="4241,60"
+                        description="Dolar Turismo"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
