8a0561eda8473a72552ce9a445b930f03facd6d8
Jojo <sirjoandersonwf@gmail.com>
2022-03-23 08:47:01 -0300

    add: cors

--- backend/src/config/express.config.ts
+++ backend/src/config/express.config.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import cors from 'cors';
+
+import corsOptionsDelegate from './cors.config';
 
 import appRoute from '../routes/app.route';
 import userRoute from '../routes/user.route';
@@ -8,6 +11,8 @@ const app: express.Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors(corsOptionsDelegate));
 
 app.use(appRoute);
 app.use(userRoute);
