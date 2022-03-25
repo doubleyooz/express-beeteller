4e3323e2ddbca85adede6a6a3d1d5a37e4665cc6
Jojo <sirjoandersonwf@gmail.com>
2022-03-24 22:40:45 -0300

    add: tokenVersion

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -3,11 +3,16 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IUser extends Document {
     email: string;
     password: string;
+    tokenVersion: number;
 }
 
-const UserSchema: Schema = new Schema({
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true, select: false },
-}, {timestamps: true});
+const UserSchema: Schema = new Schema(
+    {
+        email: { type: String, unique: true, required: true },
+        password: { type: String, required: true, select: false },
+        tokenVersion: { type: Number, default: 0 },
+    },
+    { timestamps: true },
+);
 
 export default mongoose.model<IUser>('User', UserSchema);
