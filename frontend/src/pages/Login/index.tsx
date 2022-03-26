881879f476982e5b5db32b3dcb1fa3cbedd77f9e
Jojo <sirjoandersonwf@gmail.com>
2022-03-26 17:18:09 -0300

    update: Login styles && add: error message

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import api from '../../services';
 import './styles.scss';
 
 const LoginPage = () => {
+    const schema = yup.object().shape({
+        email: yup
+            .string()
+            .email('Email should have correct format')
+            .required('Email is a required field'),
+        password: yup
+            .string()
+            .min(8, 'Password is too short - must be 8 chars minimum.')
+            .matches(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+                'the password must contain at least 1 number, at least 1 lower case letter, at least 1 upper case and at least 1 special character.'
+            ),
+    });
+
+    type User = {
+        email: string;
+        password: string;
+    };
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<User>({
+        resolver: yupResolver(schema),
+        mode: 'onBlur',
+    });
+
+    const onSubmit = handleSubmit((data: User) => {
+        api.get(`/sign-in`, {
+            auth: {
+                username: data.email,
+                password: data.password,
+            },
+        })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+                console.log('get info failed');
+            });
+    });
     return (
         <div className="login-container">
             <div className="image"></div>
@@ -13,14 +60,25 @@ const LoginPage = () => {
                         Faça Login com seus dados inseridos durante o registro.
                     </span>
                 </div>
-                <form action="">
+                <form onSubmit={onSubmit}>
                     <div className="field">
                         <label htmlFor="email">E-mail</label>
                         <input
+                            {...register('email')}
                             type="email"
                             name="email"
                             placeholder="Example@email.com"
+                            style={
+                                errors.email
+                                    ? { borderColor: '#ff0000' }
+                                    : {}
+                            }
                         />
+                        {errors.email && (
+                            <div className="error">
+                                <span>{errors.email.message}</span>
+                            </div>
+                        )}
                     </div>
                     <div className="field">
                         <div className="field up">
@@ -29,13 +87,24 @@ const LoginPage = () => {
                         </div>
 
                         <input
+                            {...register('password')}
                             type="password"
                             name="password"
                             placeholder="Enter password"
+                            style={
+                                errors.password
+                                    ? { borderColor: '#ff0000' }
+                                    : {}
+                            }
                         />
+                        {errors.password && (
+                            <div className="error">
+                                <span>{errors.password.message}</span>
+                            </div>
+                        )}
                     </div>
 
-                    <input type="submit" value="Login" />
+                    <input className="submit" type="submit" value="Login" />
                 </form>
             </div>
         </div>
