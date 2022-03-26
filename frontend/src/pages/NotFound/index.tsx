32fcb1b2d336e814a9f617b15fc62f4d6b84cd44
Jojo <sirjoandersonwf@gmail.com>
2022-03-26 18:45:53 -0300

    add: fadeout animation

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -1,20 +1,28 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import './styles.scss';
 
 const NotFound = () => {
+    const nav = useNavigate();
+
+    useEffect(() => {
+        setTimeout(function () {
+            nav('/');
+        }, 2000);
+    }, []); // <-- empty dependency array
+
     return (
         <div className="notfound-container">
-            <div>
+            <div className="fadeout">
                 <span className="error-message-title">
                     There's nothing here.
                 </span>
-                <span className="error-message-body">                   
+                <span className="error-message-body">
                     Whatever you were looking for doesn't currently exist at
                     this address. Unless you were looking for this error page,
                     in which case: Congrats! You totally found it.
-                </span>              
+                </span>
             </div>
         </div>
     );
