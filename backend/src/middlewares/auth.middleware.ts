7f77de83f448d9fe603dce691e6d9487eee44e1c
Jojo <sirjoandersonwf@gmail.com>
2022-03-27 23:33:54 -0300

    fix: jwt errors are not catched

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -6,67 +6,55 @@ import { getMessage } from '../utils/message.util';
 
 export const auth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const [, token] = req.headers.authorization
-                ? req.headers.authorization.split(' ')
-                : [, ''];
+        const [, token] = req.headers.authorization
+            ? req.headers.authorization.split(' ')
+            : [, ''];
 
-            let payload: any = null;
-            try {
-                payload = jwt.verifyJwt(token, 1);
-            } catch (err) {
+        let payload: any = null;
+        try {
+            payload = jwt.verifyJwt(token, 1);
+        } catch (err) {
+            if(err instanceof Error)
+                throw new Error(err.message);
+            else throw new Error( getMessage('default.unauthorized'));
+        }
+
+        User.exists({
+            _id: payload._id,
+            tokenVersion: payload.tokenVersion,
+        })
+            .then(result => {
+                if (result === null) {
+                    return res.status(401).json({
+                        message: getMessage('default.unauthorized'),
+                    });
+                }
+                req.auth = payload._id;
+                let current_time = Date.now().valueOf() / 1000;
+                if (
+                    (payload.exp - payload.iat) / 2 >
+                    payload.exp - current_time
+                ) {
+                    let newToken = jwt.generateJwt(
+                        {
+                            _id: payload.id,
+                            tokenVersion: payload.tokenVersion,
+                        },
+                        1,
+                    );
+                    req.new_token = `Bearer ${newToken}`;
+                    console.log(`New Token: ${newToken}`);
+                }
+                console.log('shall pass');
+                payload = null;
+                next();
+            })
+            .catch(err => {
                 console.log(err);
-                //Invalid Token
-                return res.status(401).json({
-                    message: getMessage('default.unauthorized'),
+                return res.status(400).json({
+                    message: getMessage('default.badRequest'),
                     err: err,
                 });
-            }
-
-            User.exists({
-                _id: payload._id,
-                tokenVersion: payload.tokenVersion,
-            })
-                .then(result => {
-                    if(result === null) {
-                        return res.status(401).json({
-                            message: getMessage('default.unauthorized')
-                            
-                        });
-                    }
-                    req.auth = payload._id;
-                    let current_time = Date.now().valueOf() / 1000;
-                    if (
-                        (payload.exp - payload.iat) / 2 >
-                        payload.exp - current_time
-                    ) {
-                        let newToken = jwt.generateJwt(
-                            {
-                                _id: payload.id,
-                                tokenVersion: payload.tokenVersion,
-                            },
-                            1,
-                        );
-                        req.new_token = `Bearer ${newToken}`;
-                        console.log(`New Token: ${newToken}`);
-                    }
-
-                    payload = null;
-                    next();
-                })
-                .catch(err => {
-                    console.log(err);
-                    return res.status(400).json({
-                        message: getMessage('default.badRequest'),
-                        err: err,
-                    });
-                });
-        } catch (err) {
-            console.log(err);
-            return res.status(401).json({
-                message: getMessage('default.unauthorized'),
-                err: err,
             });
-        }
     };
 };
